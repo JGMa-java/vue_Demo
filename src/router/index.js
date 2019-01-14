b97b8e8f9f0eaa6007d3cbd2f23d48b67be4caf2
jgma <1520458127@qq.com>
2019-01-14 16:49:36 +0800

    嵌套路由配置loading组件

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import test from '@/components/test'
 import demo4 from '@/components/demo4'
-
+import Loading from '@/components/loading'
 Vue.use(Router)
 
 export default new Router({
@@ -13,15 +13,26 @@ export default new Router({
       name: 'HelloWorld',
       component: demo4
     },
-	  {
+    {
       path: '/test',
       name: 'test',
       component: test
     },
     {
-      path:'/index',
-      name:'index',
-      component:HelloWorld
+      path: '/index',
+      name: 'index',
+      component: HelloWorld,
+      children: [
+        // loadingView will be rendered inside HelloWorld's <router-view>
+        // when /index/loading is matched
+        { path: 'loading', component: Loading },
+        // ...other sub routes
+      ]
+    },
+    {
+      path:'/loading',
+      name:'loading',
+      component:Loading
     }
   ]
 })
